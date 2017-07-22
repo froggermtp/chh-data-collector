e7fe93b3389c728d1e4bd33dffc34abadda92446
Matthew Parris <mfrogparris@gmail.com>
2017-07-22 14:56:17 -0500

    Add logger
    Added a logger and logged some stuff.

--- src/main/java/com/froggermtp/chh_data_collector/UrlQueue.java
+++ src/main/java/com/froggermtp/chh_data_collector/UrlQueue.java
@@ -3,13 +3,20 @@ package com.froggermtp.chh_data_collector;
 import java.util.ArrayDeque;
 import java.util.HashSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UrlQueue {
+	private static final Logger logger = LoggerFactory.getLogger(UrlQueue.class);
+	
 	private ArrayDeque<String> urlsToCrawl = new ArrayDeque<>();
 	private HashSet<String> visitedUrls = new HashSet<>();
 	
 	public void addUrl(String url) {
 		if(!visitedUrls.contains(url)) {
 			urlsToCrawl.add(url);
+			
+			logger.debug("Url added to queue: {}", url);
 		}
 	}
 	
