65f7b8d096fe15cd8a033383ef38816c8565e223
Matthew Parris <mfrogparris@gmail.com>
2017-07-22 01:50:28 -0500

    Add UrlQueue
    The data structures that tracked the links to visit and the links that
    have already been visited were abstracted away into their own class
    file.

---
+++ src/main/java/com/froggermtp/chh_data_collector/UrlQueue.java
@@ -0,0 +1,35 @@
+package com.froggermtp.chh_data_collector;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+
+public class UrlQueue {
+	private ArrayDeque<String> urlsToCrawl;
+	private HashSet<String> visitedUrls;
+	
+	public UrlQueue() {
+		this.urlsToCrawl = new ArrayDeque<>();
+		this.visitedUrls = new HashSet<>();
+	}
+	
+	public void addUrl(String url) {
+		if(!visitedUrls.contains(url)) {
+			urlsToCrawl.add(url);
+		}
+	}
+	
+	public String getUrl()  {
+		if(!urlsToCrawl.isEmpty()) {
+			visitedUrls.add(url);
+			
+			return url;
+		}
+		
+		return null;
+	}
+	
+	public boolean isEmpty() {
+		return urlsToCrawl.isEmpty();
+	}
+	
+}
