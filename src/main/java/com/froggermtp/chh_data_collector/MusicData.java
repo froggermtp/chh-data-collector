752c65c897ff5d5672497442ab15db5d92392056
Matthew Parris <mfrogparris@gmail.com>
2017-07-26 15:07:19 -0500

    Remove ScrapedDataFactory and refactor RapzillaWebCrawler class
    Removed the factory that created MusicData. It made the code more
    complicated than it should be. Moved all the static methods from
    RapzillaHelper into the RapzillaWebCrawler class. This seemed to be a
    more proper home then just having them randomly exist in their own
    class. Also added Javadoc to the RapzillaWebCrawler class.

--- src/main/java/com/froggermtp/chh_data_collector/MusicData.java
+++ src/main/java/com/froggermtp/chh_data_collector/MusicData.java
@@ -1,16 +1,18 @@
 package com.froggermtp.chh_data_collector;
 
-public final class MusicData extends ScrapedData {	
+public final class MusicData {	
+	/**
+	 * The URL from where the data was scraped.
+	 */
+	private final String url;
 	/**
 	 * The name of the song or project.
 	 */
 	private final String project;
-	
 	/**
 	 * The name of the artist.
 	 */
 	private final String artist;
-	
 	/**
 	 * The date of when the article was released that contained the music data.
 	 * This date should be close to when the music was actually released.
@@ -18,17 +20,23 @@ public final class MusicData extends ScrapedData {
 	private final String date;
 	
 	public MusicData(String url, String contentName, String artist, String date) {
-		super(url);
-		
+		this.url = url;
 		this.project = contentName;
 		this.artist = artist;
 		this.date = date;
 	}
 
 	/**
-	 * @return the contentName
+	 * @return the URL
 	 */
-	public String getContentName() {
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @return the project
+	 */
+	public String getProject() {
 		return project;
 	}
 
@@ -46,9 +54,6 @@ public final class MusicData extends ScrapedData {
 		return date;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		StringBuilder sbuf = new StringBuilder();
--- src/main/java/com/froggermtp/chh_data_collector/RapzillaWebCrawler.java
+++ src/main/java/com/froggermtp/chh_data_collector/RapzillaWebCrawler.java
@@ -1,22 +1,24 @@
 package com.froggermtp.chh_data_collector;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Provides the logic to scrape Rapzilla's music directory.
+ */
 public class RapzillaWebCrawler extends WebCrawler {
 	private static final Logger logger = LoggerFactory.getLogger(RapzillaWebCrawler.class);
 	
+	// TODO: Remove this counter
 	private int tempCounter = 0;
 
-	/* (non-Javadoc)
-	 * @see com.froggermtp.chh_data_collector.WebCrawler#shouldVisit(java.lang.String)
-	 */
 	@Override
 	public boolean shouldVisit(String url) {
 		logger.debug("Entering shouldVisit(url={})", url);
 		
-		if(url.matches("http://www.rapzilla.com/rz/music/freemp3s/\\d+.+")) {
+		if (url.matches("http://www.rapzilla.com/rz/music/freemp3s/\\d+.+")) {
 			logger.debug("Leaving shouldVisit(): true");
 			
 			return true;
@@ -27,22 +29,126 @@ public class RapzillaWebCrawler extends WebCrawler {
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.froggermtp.chh_data_collector.WebCrawler#onVisit(org.jsoup.nodes.Document)
-	 */
 	@Override
 	public void onVisit(Document doc) {
 		logger.info("Currenly visiting url: {}", doc.location());
 		
+		// TODO: Remove this counter
 		tempCounter++;
 
-		if(tempCounter == 10) {
+		if (tempCounter == 10) {
 			stop();
 		}
 
-		ScrapedData musicData = ScrapedDataFactory.getScrapedData("MusicData", doc);
+		MusicData musicData = new MusicData(
+				doc.location(), 
+				scrapeProject(doc), 
+				scrapeArtist(doc), 
+				scrapeDate(doc)
+				);
 
 		logger.info("Scraped new music data: {}", musicData.toString());
+	}
+	
+	/**
+	 * Scrapes the artist name from the web page.
+	 * 
+	 * @param doc  the {@code Document} to scrape
+	 * @return the artist name, null if name couldn't be found
+	 */
+	private String scrapeArtist(Document doc) {
+		logger.debug("Entering scrapeArtist()");
+		
+		Elements titleElements = doc.select("title");
+		
+		if(titleElements.size() != 1) {
+			logger.debug("Multiple title elements were scraped: {}" + titleElements.toString());
+			
+			logger.debug("Leaving scrapeArtist(): null");
+			return null;
+		}
+
+		String titleString = titleElements.first().toString();
+		/**
+		 * There are two different kinds of dashes.
+		 * All the dashes are converted to one type to simplify the logic.
+		 */
+		titleString = StringHelper.replaceDashes(titleString);
+		
+		if(titleString.contains("-")) {
+			String artistString = 
+					titleString.replaceAll("(<title>Free Download:\\s+)|(\\s*-.*)", "");
+			
+			logger.debug("Leaving scrapeArtist(): {}", artistString);
+			return artistString;
+		}
+		
+		logger.debug("The title tag did not contain a dash: {}" + titleString);
+		
+		logger.debug("Leaving scrapeArtist(): null");
+		return null;
+	}
+	
+	/**
+	 * Scrapes the project name from the web page.
+	 * 
+	 * @param doc  the {@code Document} to scrape
+	 * @return the project name, null if name couldn't be found
+	 */
+	private String scrapeProject(Document doc) {
+		logger.debug("Entering scrapeProject()");
+		
+		Elements titleElements = doc.select("title");
+		
+		if(titleElements.size() != 1) {
+			logger.debug("Multiple title elements were scraped: {}", titleElements.toString());
+			
+			logger.debug("Leaving scrapeProject(): null");
+			return null;
+		}
+		
+		String titleString = titleElements.first().toString();
+		/**
+		 * There are two different kinds of dashes.
+		 * All the dashes are converted to one type to simplify the logic.
+		 */
+		titleString = StringHelper.replaceDashes(titleString);
+		
+		if(titleString.contains("-")) {
+			String projectNameString = titleString.replaceAll("(.*-\\s*)|(\\s*</title>)", "");
+			
+			logger.debug("Leaving scrapeProject(): {}", projectNameString);
+			return projectNameString;
+		}
+		
+		logger.debug("The title tag did not contain a dash: {}", titleString);
+		
+		logger.debug("Leaving scrapeProject(): null");
+		return null;
+	}
+	
+	/**
+	 * Scrape the date from the web page.
+	 * 
+	 * @param doc  the {@code Document} to scrape
+	 * @return the date, null if the date couldn't be found
+	 */
+	private String scrapeDate(Document doc) {
+		logger.debug("Entering scrapeDate()");
+		
+		Elements timeElements = doc.select("time");
+		
+		if(timeElements.size() != 1) {
+			logger.debug("Multiple time elements were scraped: {}", timeElements.toString());
+			
+			logger.debug("Leaving scrapeDate(): null");
+			return null;
+		}
+		
+		String dateString = timeElements.html().replaceAll("Created:\\s*", "");
+		
+		logger.debug("Leaving scrapeDate(): {}", dateString);
+		return dateString;
 	}
 
 }
